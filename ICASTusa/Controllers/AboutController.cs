58c5a362d01b30498062d5086abae0f821aa1c34
Patrick Yee <g0th1cazn@gmail.com>
2015-10-28 12:23:21 -0600

    Developed the pages. for the home index, About pages, and added some css.
    I have developed the home index page, a shared layout, and all, but for
    the partners pages for the About Us section.

---
+++ ICASTusa/Controllers/AboutController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICASTusa.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: About
+        public ActionResult Who_We_Are()
+        {
+            return View();
+        }
+
+        public ActionResult History_of_ICAST()
+        {
+            return View();
+        }
+
+        public ActionResult The_ICAST_Team()
+        {
+            return View();
+        }
+
+        public ActionResult The_ICAST_Board()
+        {
+            return View();
+        }
+
+        public ActionResult ResourceSmart_Board(){
+            return View();
+        }
+
+        public ActionResult ICAST_Partners()
+        {
+            return View();
+        }
+    }
+}
