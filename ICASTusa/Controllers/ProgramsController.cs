4ad6b1812ed248aaae974994acfce15e11a30283
Patrick Yee <g0th1cazn@gmail.com>
2015-10-30 12:21:23 -0600

    Developed and added program section
    Addition to developed the program section, I also added the images for
    the other pages for the About Us Section.  I added the files for the
    Resources and Training page for the Programs section and added the
    YouTube Channels video links.

---
+++ ICASTusa/Controllers/ProgramsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICASTusa.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        // GET: Programs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Green_Certifications()
+        {
+            return View();
+        }
+
+        public ActionResult Training_and_Resources()
+        {
+            return View();
+        }
+    }
+}
