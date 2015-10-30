50358c147dfd449282a62e388624e816479b6f50
Patrick Yee <g0th1cazn@gmail.com>
2015-10-30 14:18:11 -0600

    Success Stories section
    Developed the Success Stories section pages. updated the shared layout,
    and actual use the routing with this section.

---
+++ ICASTusa/Controllers/Success_StoriesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICASTusa.Controllers
+{
+    public class Success_StoriesController : Controller
+    {
+        // GET: Success_Stories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Program_Success()
+        {
+            return View();
+        }
+
+        public ActionResult Case_Studies()
+        {
+            return View();
+        }
+
+        public ActionResult Awards()
+        {
+            return View();
+        }
+
+        public ActionResult Testimonials()
+        {
+            return View();
+        }
+    }
+}
