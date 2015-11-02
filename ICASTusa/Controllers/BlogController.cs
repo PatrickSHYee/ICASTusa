387cdd649268626cd54f0245e8d712ac273da81e
Patrick Yee <g0th1cazn@gmail.com>
2015-11-02 14:36:38 -0700

    Nearly done....
    Added the section Get Involved, but for the back end of the Connect with
    ICAST page.  Added the The Latest section with the News.  Having trouble
    finding a good way to use a JavaScript calendar for the events page.

---
+++ ICASTusa/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICASTusa.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ICAST_News()
+        {
+            return View();
+        }
+
+        public ActionResult ICAST_Events()
+        {
+            return View();
+        }
+    }
+}
---
+++ ICASTusa/Controllers/Get_InvolvedController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICASTusa.Controllers
+{
+    public class Get_InvolvedController : Controller
+    {
+        // GET: Get_Involved
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Jobs()
+        {
+            return View();
+        }
+
+        public ActionResult Volunteer()
+        {
+            return View();
+        }
+
+        public ActionResult Donate()
+        {
+            return View();
+        }
+
+        public ActionResult Connect_with_ICAST()
+        {
+            return View();
+        }
+    }
+}
